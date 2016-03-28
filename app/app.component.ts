5185d91ee90fac8fa63df8313de5de975a9614e8
Sneh Sagar <sagarsneh@gmail.com>
2016-03-28 17:41:30 +0530

    quick Starter Example

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>My first Angular 2 app in typescript :)</h1>'
+})
+
+export class AppComponent {}
